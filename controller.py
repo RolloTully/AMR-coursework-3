a5faa39470dbe32baa0460cc3d578c8974f6ebc9
Rollo Tully <rollotully@gmail.com>
2025-05-02 14:15:52 +0100

    update

--- controller.py
+++ controller.py
@@ -23,6 +23,8 @@ def update_gains(control_gains,ISE_delta, ITAE_delta):
     D_adjustment = np.array([-0.05 if adj>0 else 0.05 for adj in ITAE_delta ])
     control_gains[0,:] *= P_adjustment+1
     control_gains[1,:] *= D_adjustment+1
+    print("###################  Updated Gains ###################")
+    print(control_gains)
     return control_gains
 
 def ISE(error_array, time_array):
@@ -45,13 +47,13 @@ def DCM(matrix, yaw):
         [           0,            0, 0, 1]
     ])#is this pythonic, hell no, does it make it easy to read, SOOO much
     return np.matmul(transform_matrix,matrix)
-error_queue = Queue(2)
-time_queue = Queue(20)
-ISTE_error_queue = Queue(2)
-error_queue.memory = [[0,0,0,0],[0,0,0,0]]#fills the error queue so that a derivative can be computed later
-control_gains = np.array([[0.5,0.5,0.5,0.5],
+error_queue = Queue(12)
+time_queue = Queue(12)
+ISTE_error_queue = Queue(12)
+error_queue.memory = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]#fills the error queue so that a derivative can be computed later
+control_gains = np.array([[0.2,0.2,0.2,0.2],
                           [0.1,0.1,0.1,0.1]]) #Arbiraty starting PD controller gains
-review_interval = 30
+review_interval = 12
 error_history = []
 time_history = []
 old_ISE_loss = np.array([1,1,1,1])
@@ -62,8 +64,15 @@ def controller(state, target_pos, dt):
     # target_pos format: (x (m), y (m), z (m), yaw (radians))
     # dt: time step (s)
     # return velocity command format: (velocity_x_setpoint (m/s), velocity_y_setpoint (m/s), velocity_z_setpoint (m/s), yaw_rate_setpoint (radians/s))
-    state = np.array(state[[True,True,True,False,False,True]]).T#Turns the state vector in to a column vector for ease of handeling, we dont care about roll or pitch so we filter them out
+    state = np.array(state)
+    state = state[[True,True,True,False,False,True]].T#Turns the state vector in to a column vector for ease of handeling, we dont care about roll or pitch so we filter them out
     target = np.array(target_pos).T #repeats the previous step but for the target position, roll and pitch data are added for completions sake
+    error = [0,0,0,0]
+    error[:3] = target[:3]-state[:3]
+    error[3] = min([target[3]-state[3],2*np.pi - target[3]-state[3]], key = abs) #we have to compute angular error very differently, this makes sure the drone turns throught the smallest angle
+    print(target)
+    print(state)
+    print(error)
     error = target-state#the position error is comuted
     error_queue.push(error.T)#the error is pushed to the error queue
     error_history.append(error)
@@ -78,6 +87,5 @@ def controller(state, target_pos, dt):
         old_ITAE_loss = ITAE_loss
     control_var = np.sum(control_gains*np.array([error_queue.memory[1],np.diff(error_queue(),axis=0)[0]*dt]),axis=0)# the PD global contral variable is computed
     '''Local Frame'''
-    print(control_gains)
     local_commanded_move = DCM(control_var.T,state[3])#the global control variable is moved in to the local frame
     return tuple(local_commanded_move.T)#seperates out the command variables we want to use, given that roll and pitch are controlled automatically they are ignored
