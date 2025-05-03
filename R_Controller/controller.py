4acac5eeaa0b003aa705fd7625ebde3bf2e5e846
Rollo Tully <rollotully@gmail.com>
2025-05-03 18:52:57 +0100

    updated controller
    added improvements made by team mates

---
+++ R_Controller/controller.py
@@ -0,0 +1,105 @@
+# Implement a controller
+import numpy as np
+import matplotlib.pyplot as plt
+
+#========================
+# Code by Rollo
+#========================
+
+class Queue():
+    '''Implements a queue cos im not sure if were allowed to use the class'''
+    def __init__(self,length):
+        self.length = length#the length of the queue
+        self.memory = []
+
+    def push(self, var):
+        #Adds element to the queue
+        #removes the oldest element if the queue length exceeds the desired length
+        self.memory.append(var)
+        if len(self.memory) > self.length:
+            del self.memory[0]
+
+    def __call__(self):
+        #returns the memory
+        return np.array(self.memory)
+
+def update_gains(control_gains,ISE_delta, ITAE_delta):
+    #Update gains
+    P_adjustment = np.array([-0.005 if adj>0 else 0.005 for adj in ISE_delta ])
+    D_adjustment = np.array([-0.005 if adj>0 else 0.005 for adj in ITAE_delta ])
+    control_gains[0,:] *= P_adjustment+1
+    control_gains[1,:] *= D_adjustment+1
+    print("###################  Updated Gains ###################")
+    print(control_gains)
+    return control_gains
+def ISE(error_array, time_array):
+    '''Integral Square Error'''
+    #https://www.scientificbulletin.upb.ro/rev_docs_arhiva/full0e5_577239.pdf
+    return np.sum(np.square(error_array)*np.array(time_array)[:, np.newaxis],axis=0)
+
+def ITAE(error_array, time_array):
+    '''Integral Time Averaged Error'''
+    #https://www.scientificbulletin.upb.ro/rev_docs_arhiva/full0e5_577239.pdf
+    return np.sum(np.abs(error_array)*np.array(time_array)[:,np.newaxis],axis=0)
+
+def DCM(matrix, yaw):
+    '''Computed the directional cosine matrix to convert from the global to local corrdinate frame'''
+    '''the transformation matrix has been extended to pass through th euler angles so that all errors can be handled simulatiously'''
+    transform_matrix = np.array([
+        [ np.cos(yaw),  np.sin(yaw), 0, 0],
+        [-np.sin(yaw),  np.cos(yaw), 0, 0],
+        [           0,            0, 1, 0],
+        [           0,            0, 0, 1]
+    ])#is this pythonic, hell no, does it make it easy to read, SOOO much
+    return np.matmul(transform_matrix,matrix)
+error_queue = Queue(12)
+time_queue = Queue(12)
+ISTE_error_queue = Queue(12)
+error_queue.memory = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]#fills the error queue so that a derivative can be computed later
+control_gains = np.array([[0.3,0.3,0.3,0.3],
+                          [0.06,0.06,0.06,0.06]]) #Arbiraty starting PD controller gains
+review_interval = 12
+error_history = []
+time_history = []
+old_ISE_loss = np.array([1,1,1,1])
+old_ITAE_loss = np.array([1,1,1,1])
+ISE_history = []
+ITAE_history = []
+P_history = []
+D_history = []
+def controller(state, target_pos, dt):
+    global error_history, time_history, old_ISE_loss, old_ITAE_loss, control_gains
+    # state format: [position_x (m), position_y (m), position_z (m), roll (radians), pitch (radians), yaw (radians)]
+    # target_pos format: (x (m), y (m), z (m), yaw (radians))
+    # dt: time step (s)
+    # return velocity command format: (velocity_x_setpoint (m/s), velocity_y_setpoint (m/s), velocity_z_setpoint (m/s), yaw_rate_setpoint (radians/s))
+    state = np.array(state)
+    state = state[[True,True,True,False,False,True]].T#Turns the state vector in to a column vector for ease of handeling, we dont care about roll or pitch so we filter them out
+    target = np.array(target_pos).T #repeats the previous step but for the target position, roll and pitch data are added for completions sake
+    error = target - state
+    error = [0,0,0,0]
+    #error[:3] = target[:3]-state[:3]
+    #error[3] = min([target[3]-state[3],2*np.pi - target[3]-state[3]], key = abs) #we have to compute angular error very differently, this makes sure the drone turns throught the smallest angle
+    print(target)
+    print(state)
+    print(error)
+    error = target-state#the position error is comuted
+    error_queue.push(error.T)#the error is pushed to the error queue
+    error_history.append(error)
+    time_history.append(dt)
+    if len(error_history)>review_interval:
+        #Calculates losses and updates control gains
+        ISE_loss = ISE(error_history, time_history)
+        ITAE_loss = ITAE(error_history, time_history)
+        control_gains = update_gains(control_gains, ISE_loss-old_ISE_loss,ITAE_loss-old_ITAE_loss )
+        error_history, time_history = [], []
+        old_ISE_loss = ISE_loss
+        old_ITAE_loss = ITAE_loss
+        P_history.append(control_gains[0])
+        D_history.append(control_gains[1])
+        ISE_history.append(ISE_loss)
+        ITAE_history.append(ITAE_loss)
+    control_var = np.sum(control_gains*np.array([error_queue.memory[1],np.diff(error_queue(),axis=0)[0]*dt]),axis=0)# the PD global contral variable is computed
+    '''Local Frame'''
+    local_commanded_move = DCM(control_var.T,state[3])#the global control variable is moved in to the local frame
+    return tuple(local_commanded_move.T)#seperates out the command variables we want to use, given that roll and pitch are controlled automatically they are ignored
--- controller.py
+++ controller.py
@@ -1,91 +1,154 @@
-# Implement a controller
 import numpy as np
-class Queue():
-    '''Implements a queue cos im not sure if were allowed to use the class'''
-    def __init__(self,length):
-        self.length = length#the length of the queue
+import time
+
+
+#========================
+# Code by jiayee
+#========================
+
+# =======================
+# 队列结构 / Sliding Queue
+# =======================
+class Queue:
+    def __init__(self, length):
+        self.length = length
         self.memory = []
 
     def push(self, var):
-        #Adds element to the queue
-        #removes the oldest element if the queue length exceeds the desired length
         self.memory.append(var)
         if len(self.memory) > self.length:
-            del self.memory[0]
+            self.memory.pop(0)
 
     def __call__(self):
-        #returns the memory
         return np.array(self.memory)
 
-def update_gains(control_gains,ISE_delta, ITAE_delta):
-    #Update gains
-    P_adjustment = np.array([-0.05 if adj>0 else 0.05 for adj in ISE_delta ])
-    D_adjustment = np.array([-0.05 if adj>0 else 0.05 for adj in ITAE_delta ])
-    control_gains[0,:] *= P_adjustment+1
-    control_gains[1,:] *= D_adjustment+1
-    print("###################  Updated Gains ###################")
-    print(control_gains)
-    return control_gains
+# =======================
+# 工具函数 / Utility Tools
+# =======================
 
-def ISE(error_array, time_array):
-    '''Integral Square Error'''
-    #https://www.scientificbulletin.upb.ro/rev_docs_arhiva/full0e5_577239.pdf
-    return np.sum(np.square(error_array)*np.array(time_array)[:, np.newaxis],axis=0)
+# 计算积分平方误差 / Integral of Square Error
+def ISE(errors, times):
+    return np.sum(np.square(errors) * np.array(times)[:, None], axis=0)
 
-def ITAE(error_array, time_array):
-    '''Integral Time Averaged Error'''
-    #https://www.scientificbulletin.upb.ro/rev_docs_arhiva/full0e5_577239.pdf
-    return np.sum(np.abs(error_array)*np.array(time_array)[:,np.newaxis],axis=0)
+# 计算时间加权绝对误差 / Integral of Time-weighted Absolute Error
+def ITAE(errors, times):
+    return np.sum(np.abs(errors) * np.array(times)[:, None], axis=0)
 
-def DCM(matrix, yaw):
-    '''Computed the directional cosine matrix to convert from the global to local corrdinate frame'''
-    '''the transformation matrix has been extended to pass through th euler angles so that all errors can be handled simulatiously'''
-    transform_matrix = np.array([
+# 将角度规范到 [-π, π] / Normalize angle to [-pi, pi]
+def wrap_angle(angle):
+    return (angle + np.pi) % (2 * np.pi) - np.pi
+
+# 将全局坐标转换到机体坐标系 / Transform from global to local frame
+def DCM(vector, yaw):
+    transform = np.array([
         [ np.cos(yaw),  np.sin(yaw), 0, 0],
         [-np.sin(yaw),  np.cos(yaw), 0, 0],
-        [           0,            0, 1, 0],
-        [           0,            0, 0, 1]
-    ])#is this pythonic, hell no, does it make it easy to read, SOOO much
-    return np.matmul(transform_matrix,matrix)
-error_queue = Queue(12)
-time_queue = Queue(12)
-ISTE_error_queue = Queue(12)
-error_queue.memory = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]#fills the error queue so that a derivative can be computed later
-control_gains = np.array([[0.2,0.2,0.2,0.2],
-                          [0.1,0.1,0.1,0.1]]) #Arbiraty starting PD controller gains
-review_interval = 12
-error_history = []
-time_history = []
-old_ISE_loss = np.array([1,1,1,1])
-old_ITAE_loss = np.array([1,1,1,1])
+        [          0,           0, 1, 0],
+        [          0,           0, 0, 1]
+    ])
+    return transform @ vector
+
+# ==================================
+# 控制器状态变量 / Controller States
+# ==================================
+error_queue = Queue(5)                     # 误差滑动窗口 / Error queue for derivative estimation
+error_history = []                         # 错误历史 / For ISE/ITAE计算
+time_history = []                          # 时间步历史 / For ISE/ITAE计算
+
+control_gains = np.array([                 # 控制增益矩阵 / PD gains
+    [0.6, 0.6, 0.6, 0.6],                  # Kp
+    [0.1, 0.1, 0.1, 0.1]                   # Kd
+])
+
+prev_command = np.zeros(4)                 # 上一周期命令 / For smoothing
+prev_target = np.zeros(4)                  # 上一目标点 / For detecting target switch
+prev_ISE = np.ones(4)
+prev_ITAE = np.ones(4)
+
+review_interval = 30                       # 调参周期（帧） / Adaptive tuning interval
+time_elapsed = 0.0
+
+MAX_DELTA = 0.12                           # 最大变化量 / Max delta for smoothness
+MAX_OUTPUT = 0.3                           # 最大速度输出 / Max control output
+
+ISE_history = []
+ITAE_history = []
+P_history = []
+D_history = []
+
+# ==================================
+# 主控制器函数 / Main controller
+# ==================================
 def controller(state, target_pos, dt):
-    global error_history, time_history, old_ISE_loss, old_ITAE_loss, control_gains
-    # state format: [position_x (m), position_y (m), position_z (m), roll (radians), pitch (radians), yaw (radians)]
-    # target_pos format: (x (m), y (m), z (m), yaw (radians))
-    # dt: time step (s)
-    # return velocity command format: (velocity_x_setpoint (m/s), velocity_y_setpoint (m/s), velocity_z_setpoint (m/s), yaw_rate_setpoint (radians/s))
-    state = np.array(state)
-    state = state[[True,True,True,False,False,True]].T#Turns the state vector in to a column vector for ease of handeling, we dont care about roll or pitch so we filter them out
-    target = np.array(target_pos).T #repeats the previous step but for the target position, roll and pitch data are added for completions sake
-    error = [0,0,0,0]
-    error[:3] = target[:3]-state[:3]
-    error[3] = min([target[3]-state[3],2*np.pi - target[3]-state[3]], key = abs) #we have to compute angular error very differently, this makes sure the drone turns throught the smallest angle
-    print(target)
-    print(state)
-    print(error)
-    error = target-state#the position error is comuted
-    error_queue.push(error.T)#the error is pushed to the error queue
+    global error_queue, error_history, time_history
+    global control_gains, prev_command, prev_target
+    global prev_ISE, prev_ITAE, time_elapsed
+
+    # === 状态解析 / Parse current and target states ===
+    pos = np.array(state[:3])                     # 当前坐标 / current position
+    yaw = wrap_angle(state[5])                    # 当前航向角 / current yaw
+    target_xyz = np.array(target_pos[:3])
+    target_yaw = wrap_angle(target_pos[3])
+    target_all = np.array([*target_xyz, target_yaw])
+
+    # === 检测目标切换 / Target switch detection ===
+    if np.linalg.norm(target_all - prev_target) > 0.3:
+        error_history = []
+        time_history = []
+        error_queue.memory.clear()
+    prev_target = target_all.copy()
+
+    # === 误差计算 / Compute errors ===
+    error = np.array([
+        target_xyz[0] - pos[0],
+        target_xyz[1] - pos[1],
+        target_xyz[2] - pos[2],
+        wrap_angle(target_yaw - yaw)
+    ])
+    error[np.abs(error) < 0.002] = 0.0
+
+    error_queue.push(error.tolist())
     error_history.append(error)
     time_history.append(dt)
-    if len(error_history)>review_interval:
-        #Calculates losses and updates control gains
-        ISE_loss = ISE(error_history, time_history)
-        ITAE_loss = ITAE(error_history, time_history)
-        control_gains = update_gains(control_gains, ISE_loss-old_ISE_loss,ITAE_loss-old_ITAE_loss )
+
+    # === 增益调参逻辑 / Gain adaptation based on ISE & ITAE ===
+    if len(error_history) >= review_interval:
+        ise_now = ISE(error_history, time_history)
+        itae_now = ITAE(error_history, time_history)
+
+        delta_ise = ise_now - prev_ISE
+        delta_itae = itae_now - prev_ITAE
+
+        P_adj = np.where(np.abs(delta_ise) > 0.01, np.where(delta_ise > 0, -0.005, 0.003), 0.0)
+        D_adj = np.where(np.abs(delta_itae) > 0.01, np.where(delta_itae > 0, -0.007, 0.004), 0.0)
+
+        control_gains[0] = np.clip(control_gains[0] * (1 + P_adj), 0.1, 1.5)
+        control_gains[1] = np.clip(control_gains[1] * (1 + D_adj), 0.05, 1.0)
+
+        prev_ISE, prev_ITAE = ise_now, itae_now
         error_history, time_history = [], []
-        old_ISE_loss = ISE_loss
-        old_ITAE_loss = ITAE_loss
-    control_var = np.sum(control_gains*np.array([error_queue.memory[1],np.diff(error_queue(),axis=0)[0]*dt]),axis=0)# the PD global contral variable is computed
-    '''Local Frame'''
-    local_commanded_move = DCM(control_var.T,state[3])#the global control variable is moved in to the local frame
-    return tuple(local_commanded_move.T)#seperates out the command variables we want to use, given that roll and pitch are controlled automatically they are ignored
+
+        P_history.append(control_gains[0])
+        D_history.append(control_gains[1])
+        ISE_history.append(ISE_loss)
+        ITAE_history.append(ITAE_loss)
+
+    # === 导数估计 / Estimate derivative ===
+    error_array = error_queue()
+    deriv = np.mean(np.diff(error_array, axis=0), axis=0) / dt if len(error_array) >= 2 else np.zeros(4)
+
+    # === 控制器输出 / PD control law ===
+    control_signal = control_gains[0] * error + control_gains[1] * deriv
+
+    # === 平滑变化 / Smooth command transition ===
+    local_command = DCM(control_signal, yaw)
+    delta_cmd = np.clip(local_command - prev_command, -MAX_DELTA, MAX_DELTA)
+    smoothed_cmd = prev_command + delta_cmd
+    prev_command[:] = smoothed_cmd
+
+    # === 限幅输出 / Clip to max speed ===
+    vx, vy, vz, yaw_rate = np.clip(smoothed_cmd, [-MAX_OUTPUT]*3 + [-1.0], [MAX_OUTPUT]*3 + [1.0])
+
+    time_elapsed += dt
+
+    return float(vx), float(vy), float(vz), float(yaw_rate)
